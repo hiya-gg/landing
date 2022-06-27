d085e8e159af655b8b23202dde25a9bb29866afc
Koding Dev <kodingdev@pm.me>
2022-06-27 12:21:13 +1000

    fix(package.json): linting scripts

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,10 +22,7 @@ const links = [
 export default function HomePage() {
   return (
     <>
-      <NextSeo
-        title="hiya!"
-        description="Hiya.gg is a social platform to connect all your accounts together."
-      />
+      <NextSeo title="hiya!" description="Hiya.gg is a social platform to connect all your accounts together." />
 
       <div
         className="flex h-screen w-screen"
@@ -40,11 +37,7 @@ export default function HomePage() {
             {links.map((link) => (
               <Link key={link.label} href={link.href} passHref>
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a
-                  className="text-neutral-500 hover:text-neutral-300 transition-all"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a className="text-neutral-500 hover:text-neutral-300 transition-all" target="_blank" rel="noreferrer">
                   {link.label}
                 </a>
               </Link>
@@ -52,13 +45,8 @@ export default function HomePage() {
           </div>
 
           <div>
-            <h1 className="text-7xl font-bold mb-4 hover:scale-[102%] transition-all origin-left">
-              hiya! 👋
-            </h1>
-            <h2 className="text-3xl font-medium text-neutral-500 mb-4">
-              let&apos;s connect, together.
-            </h2>
-
+            <h1 className="text-7xl font-bold mb-4 hover:scale-[102%] transition-all origin-left">hiya! 👋</h1>
+            <h2 className="text-3xl font-medium text-neutral-500 mb-4">let&apos;s connect, together.</h2>
             <p className="text-md text-neutral-700">Coming soon.</p>
           </div>
 
@@ -68,12 +56,8 @@ export default function HomePage() {
                 <Logo className="group-hover:rotate-[30deg] transition-all" />
 
                 <div className="flex flex-col">
-                  <p className="text-sm text-neutral-600">
-                    Copyright &copy; {new Date().getFullYear()}
-                  </p>
-                  <p className="text-sm font-medium text-neutral-500">
-                    Zerite Development
-                  </p>
+                  <p className="text-sm text-neutral-600">Copyright &copy; {new Date().getFullYear()}</p>
+                  <p className="text-sm font-medium text-neutral-500">Zerite Development</p>
                 </div>
               </div>
             </a>
